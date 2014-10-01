ef341ada778d83baf1eb717d1cb5ee1c19bedf51
bdusell <bdusell@gmail.com>
2014-10-01 00:08:44 -0700

    Show followers and followed users pages.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe "User pages" do
 
   subject { page }
 
@@ -206,6 +206,34 @@ describe "UserPages" do
 
     it 'should not show delete links by another user\'s posts' do
       expect(page).not_to have_selector('a', text: 'delete')
+    end
+  end
+
+  describe 'following/followers' do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:other_user) { FactoryGirl.create(:user) }
+    before { user.follow!(other_user) }
+
+    describe 'followed users' do
+      before do
+        sign_in user
+        visit following_user_path(user)
+      end
+
+      it { should have_title(full_title('Following')) }
+      it { should have_selector('h3', text: 'Following') }
+      it { should have_link(other_user.name, href: user_path(other_user)) }
+    end
+
+    describe 'followers' do
+      before do
+        sign_in other_user
+        visit followers_user_path(other_user)
+      end
+
+      it { should have_title(full_title('Followers')) }
+      it { should have_selector('h3', text: 'Followers') }
+      it { should have_link(user.name, href: user_path(user)) }
     end
   end
 end
